6d2e7fc76b340fcef306001708fcadbb63ebd7be
vlad grisko <grisvladko@gmail.com>
2020-10-18 20:26:00 +0300

    Initial Commit

---
+++ COVIDStatisticsGov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVIDStatisticsGov
+//
+//  Created by hyperactive on 18/10/2020.
+//  Copyright © 2020 hyperactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
